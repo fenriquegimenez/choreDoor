53221d02df8cedd5a2ce75f1dbb48bead6cfe51b
Enrique Gimenez <fenriquegimenez@gmail.com>
2020-05-21 23:27:14 -0400

    script developed

--- script.js
+++ script.js
@@ -6,10 +6,11 @@ const botDoorPath = 'https://s3.amazonaws.com/codecademy-content/projects/chore-
 const beachDoorPath = 'https://s3.amazonaws.com/codecademy-content/projects/chore-door/images/beach.svg'
 const spaceDoorPath = 'https://s3.amazonaws.com/codecademy-content/projects/chore-door/images/space.svg'
 
-const numClosedDoors = 3
-const openDoor1;
-const openDoor2;
-const openDoor3;
+let numClosedDoors = 3;
+let openDoor1;
+let openDoor2;
+let openDoor3;
+
 const randomChoreDoorGenerator = () => {
   const choreDoor = Math.floor(Math.random() * numClosedDoors);
   if (choreDoor === 1) {
@@ -29,6 +30,57 @@ const randomChoreDoorGenerator = () => {
   }
 }
 
-randomChoreDoorGenerator();
+const isclicked = (door) => {
+	if (door.src === closedDoorPath) {
+	  return false
+	} else {
+	  return true
+	}
+}
+const isBot = (door) => {
+  if (door.src = botDoorPath) {
+    return true;
+  } else {
+    return false;
+  }
+}  
 
+doorImage1.onclick = () => {
+  if (currentlyPlaying && !isClicked(door)) {
+    doorImage1.src = botDoorPath;
+    playDoor(doorImage1);
+  }
+}
+doorImage2.onclick = () => {
+  if (currentlyPlaying && !isClicked(door)) {
+    doorImage2.src = beachDoorPath;
+    playDoor(doorImage2);
+  }
+}
+doorImage3.onclick = () => {
+    if (currentlyPlaying && !isClicked(door)) {
+      doorImage3.src = spaceDoorPath;
+      playDoor(doorImage3);
+    }
+}
+const startButton = document.getElementById('start')
 
+const gameOver = (status) => {
+  if (status === 'win') {
+    startButton.innerHTML = 'You win! Play again?'
+  } else {
+    startButton.innerHTML = 'Game over! Play again?'
+  }
+} 
+let playDoor = (door) => {
+  numClosedDoors--;
+  if (numClosedDoors === 0) {
+    gameOver('win');
+  } else if(isBot (door)) {
+    gameOver();
+  }
+} 
+
+const closedDoorPath = 'https://s3.amazonaws.com/codecademy-content/projects/chore-door/images/closed_door.svg'
+
+let currentlyPlaying = true;
